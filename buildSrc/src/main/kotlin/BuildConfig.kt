a39c24954a6944d604e1f5b93cf1c24ea82e2b4e
ulises-agr <gonzalezua@hitss.com>
2026-04-23 11:43:40 -0600

    Updates

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -6,7 +6,7 @@ object BuildConfig {
 
     const val APP_NAMESPACE = "com.myalbum2026.mobile"
     const val APP_ID = "com.myalbum2026.mobile"
-    const val MIN_SDK_VERSION = 29
+    const val MIN_SDK_VERSION = 24
     const val TARGET_SDK_VERSION = 36
     const val COMPILE_SDK_VERSION = 36
 }
--- buildSrc/src/main/kotlin/ReleaseConfig.kt
+++ buildSrc/src/main/kotlin/ReleaseConfig.kt
@@ -4,6 +4,6 @@
  */
 object ReleaseConfig {
 
-    const val VERSION_CODE = 3
-    const val VERSION_NAME = "1.2"
+    const val VERSION_CODE = 4
+    const val VERSION_NAME = "1.2.1"
 }
