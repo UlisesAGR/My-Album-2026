46522195ae3c33725d8e8c48c01e5c7e608ff41b
ulises-agr <gonzalezua@hitss.com>
2026-04-24 16:57:57 -0600

    Added empty state

--- app/src/main/java/com/myalbum2026/mobile/presenter/ui/dashboard/countries/viewmodel/CountryListUiState.kt
+++ app/src/main/java/com/myalbum2026/mobile/presenter/ui/dashboard/countries/viewmodel/CountryListUiState.kt
@@ -8,5 +8,5 @@ import com.myalbum2026.mobile.domain.model.CardsItem
 
 data class CountryListUiState(
     val isLoading: Boolean = false,
-    val teams: List<CardsItem.TeamHeader> = emptyList(),
+    val teams: List<CardsItem.TeamHeader>? = null,
 )
